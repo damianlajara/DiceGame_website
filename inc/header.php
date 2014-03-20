91c067342c9f97ed3564435113e4e0246ad09593
Damian Lajara <damiansito93@gmail.com>
2014-03-20 09:02:49 -0400

    Created header

---
+++ inc/header.php
@@ -0,0 +1,9 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Dice Game</title>
+	<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/style.css" type="text/css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
+	<!--<script type="text/javascript" src="my_script.js"></script>-->
+</head>
+<body>
