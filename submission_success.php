a94e2932b585f5a5174e3758d0c9548699f56f62
Damian Lajara <damiansito93@gmail.com>
2014-03-21 05:23:59 -0400

    fixed form error and added a link to redirect back to form if error

--- submission_success.php
+++ submission_success.php
@@ -1,22 +1,77 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"]=="POST") 
 {
-	//Trim() deletes trailing whitespace from the beginning and end of the string
-	//ie: If someone hits enter, instead of entering a value or enters a space, the trim will take care of that
-	$firstname = trim($_POST["firstname"]);
-	$lastname = trim($_POST["lastname"]);
-	$name = $firstname . " " . $lastname;
-	$email = trim($_POST["email"]);
-	$username = trim($_POST["username"]);
-	$password = trim($_POST["password"]);
-
-
-    if($firstname == "" OR $lastname == "" OR $email == "" OR $username = "" OR $password = "")
+	/*if($_POST["firstname"] == "" OR $_POST["lastname"] == "" OR $_POST["email"] == "" OR $_POST["username"] == "" OR $_POST["password"] == "")
     {//If name or email doesnt have a value...
-        $error_message = "You must specify a value for name, email address and message.";
+        $error_message = "You must specify a value for firstname, lastname, email address, username and password.";
         echo $error_message;
         exit();
+    }*/
+    if($_POST["firstname"] == "") {
+    	$error_message = "You must specify a value for firstname";
+        echo $error_message;
+        echo "<br />";
     }
+    if($_POST["lastname"] == "") {
+    	$error_message = "You must specify a value for lastname";
+        echo $error_message;
+        echo "<br />";
+    }
+    if($_POST["email"] == "") {
+    	$error_message = "You must specify a value for email address";
+        echo $error_message;
+        echo "<br />";
+    }
+    if($_POST["username"] == "") {
+    	$error_message = "You must specify a value for username";
+        echo $error_message;
+        echo "<br />";
+    }
+    if($_POST["password"] == "") {
+    	$error_message = "You must specify a value for password";
+        echo $error_message;
+        echo "<br />";
+    }
+    if(isset($error_message)) { ?>
+    	<a href="index.php" id="my_link">Go back to form</a><!--make it possible to have the data in the form already when you go back!>
+    <?php 
+    	exit();
+    }
+
+	if(isset($_POST["firstname"])) {
+		$firstname = trim($_POST["firstname"]);
+	} else {
+		$firstname = "Not Specified!";
+	}
+	if(isset($_POST["lastname"])) {
+		$lastname = trim($_POST["lastname"]);
+	} else {
+		$lastname = "Not Specified!";
+	}
+	if(isset($_POST["email"])) {
+		$email = trim($_POST["email"]);
+	} else {
+		$email = "Not Specified!";
+	}
+	if(isset($_POST["username"])) {
+		$username = trim($_POST["username"]);
+	} else {
+		$username = "Not Specified!";
+	}
+	if(isset($_POST["password"])) {
+		$password = trim($_POST["password"]);
+	} else {
+		$password = "Not Specified!";
+	}
+	//var_dump($_POST);
+	//Trim() deletes trailing whitespace from the beginning and end of the string
+	//ie: If someone hits enter, instead of entering a value or enters a space, the trim will take care of that
+	
+	if(isset($_POST["firstname"]) AND isset($_POST["lastname"])) {
+	$name = $firstname . " " . $lastname;
+	} else {
+		$name = $name;
+	}
 
     if(!isset($error_message))
     {
@@ -32,7 +87,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST")
         }
     }
 	//If it has not already encountered an error and If it is not blank, then we assume an evil hacker robot is trying to hack our page
-    if(!isset($error_message) AND ($_POST["firstname"] !== "" AND $_POST["lastname"] !== "" AND $_POST["email"] !== "" AND $_POST["username"] !== "" AND $_POST["password"] !== ""))
+    if(isset($error_message) AND ($_POST["firstname"] !== "" AND $_POST["lastname"] !== "" AND $_POST["email"] !== "" AND $_POST["username"] !== "" AND $_POST["password"] !== ""))
     {
         $error_message = "Your form submission has an error.";
         echo $error_message;
