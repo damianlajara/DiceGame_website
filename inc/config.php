381dce2f7815d7e892dee62df51fcef646546ffa
Damian Lajara <damiansito93@gmail.com>
2014-03-25 11:35:56 -0400

    deleted everything, to start from scratch

--- inc/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define("BASE_URL","/DiceGame_website/");//creates a constant for the base url, which every file is listed under
-define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/");//Server path to the homepage of the website
-//$_SERVER["DOCUMENT_ROOT"] = "/Applications/MAMP/htdocs/DiceGame_website"
